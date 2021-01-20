943aba343ce8ad544e69e360d54246d79bb7f3b8
Andhar7 <70223638+Andhar7@users.noreply.github.com>
2021-01-20 19:20:55 +0100

    Initial Commit

---
+++ WatchesApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchesApp
+//
+//  Created by Andrej Kling on 20.01.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WatchesApp/WatchesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchesAppApp.swift
+//  WatchesApp
+//
+//  Created by Andrej Kling on 20.01.21.
+//
+
+import SwiftUI
+
+@main
+struct WatchesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
